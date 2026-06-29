efc76b6716a22e331fccf77efd81782124e39f21
João Victor <ribeiroo1109@gmail.com>
2026-06-29 19:22:16 -0300

    Conceito e exemplos de comentários e DocStrings

---
+++ 01_introduction/01_comentarios.py
@@ -0,0 +1,18 @@
+# Permite escrever um comentário em uma única linha.
+
+print(1234)  # Exibe o número 1234 no terminal.
+
+
+"""
+DocString
+
+Pode ser usada para documentar módulos, funções, classes e métodos.
+Também pode servir para escrever notas durante os estudos.
+ 
+"""
+
+# Ou
+
+'''
+Outra forma de escrever uma DocString.
+'''
