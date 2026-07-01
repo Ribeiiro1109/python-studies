20a4ae3c73696977dd5bf358ad87edcdfab66d9b
João Victor <ribeiroo1109@gmail.com>
2026-07-01 20:28:52 -0300

    Adiciona exemplos dos tipos int, float e uso de type()

---
+++ 01_introduction/04_tipos_int_float.py
@@ -0,0 +1,30 @@
+"""
+Python - Tipos de dados: int e float
+
+int (integer) representa números inteiros, ou seja, valores sem parte decimal.
+Esses valores podem ser positivos, negativos ou zero.
+
+float representa números de ponto flutuante, ou seja, valores com parte decimal.
+Assim como o tipo int, os valores podem ser positivos, negativos ou zero.
+"""
+
+# Exemplos de números inteiros (int).
+print(10)
+print(-11)
+print(0)
+
+# Exemplos de números de ponto flutuante (float).
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+# A função type() retorna o tipo do objeto informado.
+# O Python identifica automaticamente (inferência de tipos)
+# qual é o tipo de cada valor.
+
+print(type(''))
+print(type(0))
+print(type(-1))
+print(type(0.0))
+print(type(-1.1))
+
+
