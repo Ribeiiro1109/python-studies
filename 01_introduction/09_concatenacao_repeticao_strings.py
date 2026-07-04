8b2b87bcf1cf3b5cf1c4104026ab00f530a39a3e
João Victor <ribeiroo1109@gmail.com>
2026-07-04 13:51:57 -0300

    Adiciona exemplos de concatenação e repetição de strings

---
+++ 01_introduction/09_concatenacao_repeticao_strings.py
@@ -0,0 +1,19 @@
+"""
+Python - Concatenação e Repetição de Strings
+
+Strings podem ser combinadas utilizando operadores.
+
+- O operador (+) realiza a concatenação de strings.
+- O operador (*) permite repetir uma string várias vezes.
+"""
+
+# O operador (+) une duas ou mais strings em uma única string.
+concatenacao = "João" + " " + "Victor"
+print(concatenacao)
+
+# O operador (*) repete uma string a quantidade de vezes informada.
+a_dez_vezes = "A" * 10
+tres_vezes_joao = 3 * "João"
+
+print(a_dez_vezes)
+print(tres_vezes_joao)
